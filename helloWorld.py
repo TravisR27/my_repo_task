590ff8cb3061cb5656227dc2bb2a6204021cff2c
Travis Rogers <153740421+TravisR27@users.noreply.github.com>
2024-03-12 12:58:13 +0000

    Update helloWorld.py
    change of variable names

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,8 @@
-string = 'Hello World'
-print (string)
+phrase = 'Hello World'
+print (phrase)
 
-string1 = 'git is awesome'
-print (string1)
+new_phrase = 'git is awesome'
+print (new_phrase)
 
 username = input("Enter username")
 print ("Username is: " + username)
