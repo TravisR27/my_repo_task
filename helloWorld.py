1e199207a76b019c279d893825e8c575abce60c2
Travis Rogers <153740421+TravisR27@users.noreply.github.com>
2024-03-12 13:06:37 +0000

    Update2 helloWorld.py
    Variable name changes

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,18 @@
+# Task 1 - Creating of program helloWorld, with a committed change to add a new Variable.
+
+# Variable (string) used to store input. 
 phrase = 'Hello World'
-print (phrase)
+# Output of print command will be 'Hello World'.
+print (string)
 
+# Variable change committed to code, with a new input.
 new_phrase = 'git is awesome'
-print (new_phrase)
+# Output of print command will be 'git is awesome'
+print (string1)
 
+# Task 2 - Asking the user to add their input.
+
+# Variable requesting for user to enter a input.
 username = input("Enter username")
+# Output of print command will be the user inputted message.
 print ("Username is: " + username)
